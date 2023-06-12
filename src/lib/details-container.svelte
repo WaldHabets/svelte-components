f93a18021a252d3ea4876581d9fd7d03ff99dc3c
wald-habets <mail@waldhabets.eu>
2023-06-12 13:33:47 +0200

    updated details container and added checkboxes to index

--- src/lib/details-container.svelte
+++ src/lib/details-container.svelte
@@ -15,7 +15,7 @@
   }
 </script>
 
-<div class={"details-container-wrapper container " + bClass}>
+<div class={"details-container-wrapper " + (open ? "--expanded" : "") + bClass}>
   <div class={"summary-container " + bClassSummary}>
     <div class="summary">
       <slot name="summary" />
@@ -43,45 +43,5 @@
 </div>
 
 <style lang="scss">
-  .details-container-wrapper {
-    padding: 0;
-    .summary-container {
-      padding: 4px;
-      .summary {
-        padding: 12px;
-      }
-    }
-    .details-container {
-      border-top: 1px solid #bbb;
-      background: rgba(black, 0.05);
-      padding: 4px;
-      .details {
-        padding: 12px;
-      }
-    }
-    .expand-button {
-      padding: 4px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border-radius: 16px;
-      border: none;
-      width: 100%;
-      background: none;
-      box-sizing: border-box;
-      * {
-        display: block;
-      }
-      svg {
-        width: 24px;
-        height: 24px;
-      }
-      &:hover {
-        background: rgba(black, 0.1);
-      }
-      &:active {
-        background: rgba(black, 0.3);
-      }
-    }
-  }
+
 </style>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,7 @@
 // Reexport your entry components here
 import Breadcrumbs from "./breadcrumbs.svelte";
+import Checkbox from "./checkbox.svelte";
+import CheckboxGroup from "./checkbox-group.svelte";
 import DetailsContainer from "./details-container.svelte";
 import HeaderBar from "./header-bar.svelte";
 import ListItemActive from "./list-item-active.svelte";
@@ -15,9 +17,11 @@ import SimpleContainerHeader from "./simple-container-header.svelte";
 import Tablist from "./tablist.svelte";
 import { layout } from "./layout";
 
-export default {
+export {
   layout,
   Breadcrumbs,
+  Checkbox,
+  CheckboxGroup,
   PageLayout,
   DetailsContainer,
   ListItemActive,
